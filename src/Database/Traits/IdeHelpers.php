1009d5b73523197bdefbe712d139b8922506fdc9
yesccx <>
2023-08-23 10:07:18 +0800

    chore: IdeHelpers comment

--- src/Database/Traits/IdeHelpers.php
+++ src/Database/Traits/IdeHelpers.php
@@ -15,12 +15,12 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
  * @method QueryBuilder|EloquentBuilder|static|$this whenLike(mixed $fields, mixed $keywords, int $mode = 0) (when)LIKE查询
  * @method QueryBuilder|EloquentBuilder|static|$this whereToday(mixed $field) 查询是否为当日
  * @method QueryBuilder|EloquentBuilder|static|$this whereThisDay(mixed $field) 查询是否为当日
- * @method QueryBuilder|EloquentBuilder|static|$this whereInDay(mixed $field) 查询是否为某日
+ * @method QueryBuilder|EloquentBuilder|static|$this whereInDay(mixed $field, mixed $day) 查询是否为某日
  * @method QueryBuilder|EloquentBuilder|static|$this whereThisWeek(mixed $field) 查询是否为本周
  * @method QueryBuilder|EloquentBuilder|static|$this whereThisMonth(mixed $field) 查询是否为本月
- * @method QueryBuilder|EloquentBuilder|static|$this whereInMonth(mixed $field) 查询是否为某月
+ * @method QueryBuilder|EloquentBuilder|static|$this whereInMonth(mixed $field, mixed $month) 查询是否为某月
  * @method QueryBuilder|EloquentBuilder|static|$this whereThisYear(mixed $field) 查询是否为本年
- * @method QueryBuilder|EloquentBuilder|static|$this whereInYear(mixed $field) 查询是否为某年
+ * @method QueryBuilder|EloquentBuilder|static|$this whereInYear(mixed $field, mixed $year) 查询是否为某年
  * @method QueryBuilder|EloquentBuilder|static|$this whereGtDate(mixed $field, mixed $date) 查询是否大于指定日期
  * @method QueryBuilder|EloquentBuilder|static|$this whereGteDate(mixed $field, mixed $date) 查询是否大于等于指定日期
  * @method QueryBuilder|EloquentBuilder|static|$this whereLtDate(mixed $field, mixed $date) 查询是否小于指定日期
